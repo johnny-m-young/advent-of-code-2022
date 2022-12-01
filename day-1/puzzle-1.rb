b404181ca28ff7dfd6ac42363052521fb2b29327
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2022-12-01 11:36:54 +0000

    Day 1 Puzzle 1 (Completed)

---
+++ day-1/puzzle-1.rb
@@ -0,0 +1,12 @@
+def get_max_calories
+  file = File.open("data-puzzle-1.txt")
+  calorie_data = file.read.split("\n\n")
+
+  calorie_data.map! do |elf_inventory|
+    elf_inventory.split("\n").map { |item_calories| item_calories.to_i }.sum
+  end
+
+  return calorie_data.max()
+end
+
+puts get_max_calories()
